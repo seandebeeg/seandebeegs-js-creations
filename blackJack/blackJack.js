48fbf0fafd0e2cf9a03420d2b9b3211c422bd006
Sean de beeg <seandepew2011@gmail.com>
2025-06-16 08:58:45 -0400

    replace with betterBlackJack.js code

--- blackJack/blackJack.js
+++ blackJack/blackJack.js
@@ -1,156 +1,148 @@
-let values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
-let suits = ['hearts', 'diamonds', 'spades', 'clubs'];
 let knownCards = new Set();
-let players = 2;
 let cardsPerPlayer = 2;
-let playerOne = {cards:[],chips:{oneDollar:20,fiveDollar:15,tenDollar:10,twentyFiveDollar:5,fiftyDollar:2,hundredDollar:1}, i:1, isTurn:true,className:'player1',isStanding:false};
-let playerTwo = {cards:[],chips:{oneDollar:20,fiveDollar:15,tenDollar:10,twentyFiveDollar:5,fiftyDollar:2,hundredDollar:1}, i:1,isTurn:false,className:'player2',isStanding:false};
-let playerThree = {cards:[],chips:{oneDollar:20,fiveDollar:15,tenDollar:10,twentyFiveDollar:5,fiftyDollar:2,hundredDollar:1}, i:1,isTurn:false,className:'player3',isStanding:false};
-let playerFour = {cards:[],chips:{oneDollar:20,fiveDollar:15,tenDollar:10,twentyFiveDollar:5,fiftyDollar:2,hundredDollar:1}, i:1,isTurn:false,className:'player4',isStanding:false};
-let playerFive = {cards:[],chips:{oneDollar:20,fiveDollar:15,tenDollar:10,twentyFiveDollar:5,fiftyDollar:2,hundredDollar:1}, i:1,isTurn:false,className:'player5',isStanding:false};
 let cards = [];
 let previousBetValue = 1;
+let playerNumber;
 
-function dealCards() {
-  players = document.getElementById('playerCount').value || players;
-  while (cards.length < 52) {
-    let valueGenerator = Math.floor(Math.random() * values.length);
-    let suitGenerator = Math.floor(Math.random() * suits.length);
-    let cardPicture = `<img src="deck-of-cards/${values[valueGenerator]}_of_${suits[suitGenerator]}.png">`;
-    let card = { name: values[valueGenerator], suit: suits[suitGenerator], photo:cardPicture};
-    let cardString = `${card.name} of ${card.suit}, ${card.photo}`;
+function createPlayer(className){
+  return {
+    cards: [],
+    chips: {
+      oneDollar: 20,
+      fiveDollar: 15,
+      tenDollar: 10,
+      twentyFiveDollar: 5,
+      fiftyDollar: 2,
+      hundredDollar: 1
+    },
+    isTurn: false,
+    className: className,
+    isStanding: false,
+    i: 1
+  };
+}
 
-    if (!knownCards.has(cardString)) {
-      knownCards.add(cardString);
-      cards.push(card);
+let players = [
+  createPlayer('player1'),
+  createPlayer('player2'),
+  createPlayer('player3'),
+  createPlayer('player4'),
+  createPlayer('player5')
+];
+players[0].isTurn = true;
+
+function countPlayers(){
+  playerNumber = parseInt(document.getElementById('playerCount').value);
+  return playerNumber;
+}
+
+function buildDeck() {
+  const values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+  const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+  let deck = [];
+  for (let suit of suits) {
+    for (let value of values) {
+      deck.push({ name: value, suit, photo: `<img src="deck-of-cards/${value}_of_${suit}.png">` });
     }
   }
-  for(let i=0; i<cardsPerPlayer; i++){
-   playerOne.cards.push(cards.shift())
-   playerTwo.cards.push(cards.shift())
-   if(players>2){
-    playerThree.cards.push(cards.shift())
-    if(players>3){
-      playerFour.cards.push(cards.shift())
-      if(players>4){
-        playerFive.cards.push(cards.shift())
-       }
-     }
-   } 
-  }
-  document.querySelector('.initial-page').innerHTML = ``
-  document.querySelector('.deck').innerHTML = '<img src="card-facedown.jpg" class="deck">'
-  revealCards(false);
-  document.querySelector('.player1').innerHTML = `${playerOne.cards[0].photo} <img src ="card-facedown.jpg" class="player1"><br> <button onclick="flipCard(playerOne)" class="action-button">Opening Bet:${previousBetValue}</button>`;
-  playAudio('sounds/card-flip.mp3');
-}
-function flipCard(player) {
-  player.chips.oneDollar -= previousBetValue;
-  previousBetValue += 1;
-  if(player === playerOne){
-    document.querySelector('.player1').innerHTML = `<button onclick="check(true,${previousBetValue},playerOne)" class="action-button">Meet!</button> <button onclick="check(true,${previousBetValue + 1},playerOne)" class="action-button">Raise!</button><button onclick="check(false,0,playerOne)"class="action-button">Stand!</button><br> ${playerOne.cards[0].photo} ${playerOne.cards[1].photo}`;
-  }
-  playAudio('sounds/card-flip.mp3')
+  return deck;
 }
 
-function check(didHit, betValue, player) {
-    if (!didHit){
-       let allHands = [playerOne.cards, playerTwo.cards, playerThree.cards, playerFour.cards, playerFive.cards]
-        .slice(0, players).map(cards => getHandValue({cards})); 
-      console.log(allHands)
-      player.isStanding = true;
-      const playerStandingStatus = {p1:playerOne.isStanding,p2:playerTwo.isStanding,p3:playerThree.isStanding,p4:playerFour.isStanding,p5:playerFive.isStanding}
-      const standingValues = Object.values(playerStandingStatus)
-        while(standingValues.length > players){
-          standingValues.pop()
-        }
-        allHands.forEach((hand,index) =>{
-          if(hand > 21){
-            standingValues[index] = true
-          }
-        })
-      if(standingValues.includes(false)){
-        changeTurns()
-        return;
-      } else{
-        let maxHand = Math.max(...allHands);
-        console.log(maxHand)
-        if(maxHand > 21){
-          allHands.forEach((hand, index) => {
-            if (hand > 21) {
-              allHands[index] = 1;
-              allHands.splice(index, 1);
-              console.log(allHands);
-            }
-          })
-           maxHand = Math.max(...allHands);
-           console.log(maxHand)
-        }
-        let winners = allHands
-          .map((value, index) => ({value, index}))
-          .filter(hand => hand.value === maxHand && hand.value <= 21);
-          console.log(winners.length)
-        if (winners.length > 0) {
-            document.querySelector('.results').innerHTML = 
-              `Winner(s): Player ${winners.map(w => w.index + 1).join(', ')} with ${maxHand}
-              <button class = "action-button" onclick = "resetGame();">Play Again</button>`;
-            revealCards(true)
-        }
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+}
+
+cards = buildDeck();
+shuffle(cards);
+
+function dealCards(){
+  playerNumber = countPlayers();
+  const playersPlaying = parseInt(playerNumber);
+  for (let i = 0; i < playersPlaying; i++) {
+    for (let j = 0; j < cardsPerPlayer; j++) {
+      players[i].cards.push(cards.shift());
+    }
+  }
+  document.querySelector('.initial-page').innerHTML = '';
+  document.querySelector('.deck').innerHTML = '<img src="card-facedown.jpg" class="deck">';
+  playAudio('sounds/card-flip.mp3');
+  revealCards(false);
+}
+
+function check(didHit, betValue, player){
+  if (!didHit) {
+    // Get all hand values for active players
+    let allHands = players.slice(0, playerNumber).map(p => getHandValue(p));
+    player.isStanding = true;
+
+    // Get standing status for active players
+    const standingValues = players.slice(0, playerNumber).map(p => p.isStanding);
+    // Marks busted hands as standing
+    allHands.forEach((hand, index) => {
+      if (hand > 21) {
+        standingValues[index] = true;
+        players[index].isStanding = true;
       }
-      changeTurns()
+    });
+
+    if (standingValues.includes(false)) {
+      changeTurns();
+      return;
     } else {
-      player.chips.oneDollar -= betValue;
-      if (player.chips.oneDollar <= 0 || player.chips.oneDollar < betValue) {
-        alert('You do not have enough money to bet');
-        changeTurns()
+      let maxHand = Math.max(...allHands.filter(h => h <= 21));
+      let winners = players
+        .slice(0, playerNumber)
+        .map((p, idx) => ({ value: getHandValue(p), index: idx }))
+        .filter(hand => hand.value === maxHand && hand.value <= 21);
+
+      if (winners.length > 0) {
+        document.querySelector('.results').innerHTML =
+          `Winner(s): Player ${winners.map(w => w.index + 1).join(', ')} with ${maxHand}
+          <button class="action-button" onclick="resetGame();">Play Again</button>`;
+        revealCards(true);
+      }
+    }
+    changeTurns();
+  } else {
+    player.chips.oneDollar -= betValue;
+    if (player.chips.oneDollar <= 0 || player.chips.oneDollar < betValue) {
+      alert('You do not have enough money to bet');
+      changeTurns();
+    } else {
+      player.cards.push(cards.shift());
+      player.i++;
+      if (player !== players[0]) {
+        decideForBot(player);
       } else {
-        player.cards.push(cards.shift());
-        player.i++;
-        player.cards[player.i].photo =  player.cards[player.i].photo.replaceAll('>',`class="${player.className}">`);
-        player.chips.oneDollar -= betValue;
-        previousBetValue = betValue;
-        document.querySelector(`.${player.className}`).innerHTML += player.cards[player.i].photo;
-        player.cards.forEach((card, index) => { 
-          if (card === undefined){ 
-            playerOne.cards[index].pop(); 
-          }
-        });
-        if(player !== playerOne){
-          decideForBot();
-        } else{
-          changeTurns();
-        }
+        changeTurns();
+      }
     }
   }
 }
 
 function playAudio(filePath){
-  audio = new Audio(filePath);
+  let audio = new Audio(filePath);
   audio.play().catch(err =>{
     console.error('Sound Error:',err);
   })
 }
 
 function getCardValue(card) {
-  if (card.name === 'ace') {
-    return 11;
-  } else if (['jack', 'queen', 'king'].includes(card.name)) {
-    return 10;
-  } else {
-    return parseInt(card.name);
-  }
+  if (card.name === 'ace') return 11;
+  if (['jack', 'queen', 'king'].includes(card.name)) return 10;
+  return parseInt(card.name);
 }
 
 function getHandValue(player){
   let totalValue = 0;
   let aceCount = 0;
-
   player.cards.forEach(card => {
-    let cardValue = getCardValue(card)
-    totalValue += cardValue;
-    if(card.name === 'ace'){
-      aceCount++;
-    }
+    let value = getCardValue(card);
+    totalValue += value;
+    if (card.name === 'ace') aceCount++;
   });
   
   while(totalValue > 21 && aceCount >=1){
@@ -160,137 +152,123 @@ function getHandValue(player){
   return totalValue;
 }
 
-function revealCards(isGameEnded){
-  if(!isGameEnded){
-    let createdDiv = []
-    for(let i=0; i<players; i++){
-      createdDiv.push(document.createElement('div'));
-      createdDiv[i].classList.add(`player${i+1}`);
+function revealCards(isGameEnded) {
+  if (!isGameEnded) {
+    let createdDiv = [];
+    for (let i = 0; i < playerNumber; i++) {
+      createdDiv[i] = document.createElement('div');
+      createdDiv[i].classList.add(`player${i + 1}`);
+      createdDiv[i].id = String(i + 1);
       document.body.appendChild(createdDiv[i]);
     }
-    createdDiv.forEach((div,index)=>{
-      createdDiv[index].id = String(index + 1);
-    });
-    
-    let allHands = [playerOne.cards, playerTwo.cards, playerThree.cards, playerFour.cards,playerFive.cards];
-    allHands.forEach((hand, index) => {
-      const handLength = hand.length;
-      if(handLength <= 0){
-        if(createdDiv[index]) {
-          createdDiv[index].remove();
-        }
-        delete hand;
-        return;
+
+    players.slice(0, playerNumber).forEach((player, index) => {
+      const playerDiv = document.getElementById(`${index + 1}`);
+      if (player.cards.length <= 0) {
+        playerDiv.innerHTML = '';
       } else {
-        hand.forEach(card =>{
-          card.photo = card.photo.replaceAll(">",`class="player${index+1}">`)
-        });
-        document.body.insertBefore(document.getElementById('2'),document.getElementById('deck'))
-        document.querySelector(`.player${index+1}`).innerHTML = 
-          hand[0].photo + `<img src="card-facedown.jpg" class="player${index+1}">`;
+        if(playerDiv.id == '1'){
+          playerDiv.innerHTML =  `<button onclick="check(true,${previousBetValue},players[0])" class="action-button">Meet!</button> 
+          <button onclick="check(true,${previousBetValue + 1},players[0])" class="action-button">Raise!</button>
+          <button onclick="check(false,0,players[0])"class="action-button">Stand!</button><br>`
+        }
+        playerDiv.innerHTML += player.cards.map((card, index) => {
+          if(index == 0 && player !== players[0]){
+            return `<img src="card-facedown.jpg">`;
+          }else{
+            return card.photo;
+          }
+        })
+        .join('');
       }
     });
-  } else{
-     let allHands = [playerOne.cards, playerTwo.cards, playerThree.cards, playerFour.cards,playerFive.cards];
-     allHands.forEach((hand,index) =>{
-        if(hand.length == 0){
-          allHands.splice(index)
-          console.error('Player Not Found')
+  } else {
+    players.slice(0, playerNumber).forEach((player, index) => {
+      const playerDiv = document.getElementById(`${index + 1}`);
+      if (player.cards.length === 0) {
+        if (playerDiv) playerDiv.innerHTML = '';
+      } else {
+        if (playerDiv) {
+          playerDiv.innerHTML = player.cards.map(card => card.photo).join('');
         }
-        let playerDiv = document.getElementById(`${index+1}`)
-        playerDiv.innerHTML =''
-        hand.forEach((card) =>{
-        playerDiv.innerHTML+= card.photo
-       })
-     })
+      }
+    });
   }
 }
 
 function decideForBot(player) {
-  if (!player || !player.cards || player.cards.length === 0) {
-    return;
-  }
+  if (!player || !player.cards || player.cards.length === 0) return;
   let handValue = getHandValue(player);
-  if (handValue <= 11) {
-    check(true, previousBetValue, player);
+  if (handValue <= 15) {
+    setTimeout(() => check(true, previousBetValue, player), 500);
   } else {
-    check(false, 0, player);
+    setTimeout(() => check(false, 0, player), 500);
   }
 }
 
 function changeTurns() {
-  const players = [
-    { player: playerOne, isTurn: playerOne.isTurn },
-    { player: playerTwo, isTurn: playerTwo.isTurn },
-    { player: playerThree, isTurn: playerThree.isTurn },
-    { player: playerFour, isTurn: playerFour.isTurn },
-    { player: playerFive, isTurn: playerFive.isTurn }
-  ];
+  const playersPlaying = parseInt(playerNumber);
+  const currentPlayerIndex = players.slice(0, playersPlaying).findIndex(p => p.isTurn);
 
-  const currentPlayerIndex = players.findIndex(p => p.isTurn);
   if (currentPlayerIndex >= 0) {
-    players[currentPlayerIndex].player.isTurn = false;
-    let nextIndex = (currentPlayerIndex + 1) % players.length;
-    players.forEach(player =>{
-      if(player.isStanding && nextIndex <= 5){
-        nextIndex++
-      } else if(nextIndex > 5){
-        nextIndex = 0
-      }
-    })
-    players[nextIndex].player.isTurn = true;
-    if (players[nextIndex].player !== playerOne) {
-      setTimeout(() => decideForBot(players[nextIndex].player), 500);
+    players[currentPlayerIndex].isTurn = false;
+    let nextIndex = (currentPlayerIndex + 1) % playersPlaying;
+    players[nextIndex].isTurn = true;
+    if (players[nextIndex] !== players[0]) {
+       decideForBot(players[nextIndex]);
     }
   }
 }
 
 function checkForBust(){
   let bustArray = [];
-  const allHands = [playerOne.cards, playerTwo.cards,playerThree.cards,playerFour.cards,playerFive.cards];
-  allHands.forEach(hand =>{
-    if(hand >21){
-      bustArray.push(true);
-    }else{
-      bustArray.push(false);
-    }
-  })
+  const allHands = players.map(p => getHandValue(p));
+  allHands.forEach(hand => {
+    if (hand > 21) bustArray.push(true);
+    else bustArray.push(false);
+  });
   return bustArray;
 }
 
-function resetGame(){
-  playerOne.cards = [];
-  playerTwo.cards = [];
-  playerThree.cards = []; 
-  playerFour.cards = []; 
-  playerFive.cards = [];
+function resetGame() {
+  players.forEach(player => {
+    player.cards = [];
+    player.isStanding = false;
+    player.isTurn = false;
+    player.i = 1;
+  });
+
   cards = [];
+  cards = buildDeck();
+  shuffle(cards);
   previousBetValue = 1;
-  clearDivs()
-  restoreInitialPage()
-  dealCards()
+  clearDivs();
+  restoreInitialPage();
 }
 
 function clearDivs(){
   for (let i = 1; i <= 5; i++) {
     const div = document.getElementById(`${i}`);
-    if (div) {
-      div.innerHTML = "";
-    }
+    if (div) div.innerHTML = '';
   }
 }
 
 function restoreInitialPage(){
-   document.querySelector('.initial-page').innerHTML =  `<div class="initial-page">
+  document.querySelector('.initial-page').innerHTML = `
+    <div class="initial-page">
       <h1>Pre-Game: Player Count</h1>
       <form class="playerForm">
-        <select id="playerCount">
+        <select id="playerCount" class="action-button" style="padding: 10px;">
           <option value="2">2</option>
           <option value="3">3</option>
           <option value="4">4</option>
           <option value="5">5</option>
         </select>
-        <button type ="submit" onclick="event.preventDefault(); dealCards()">Confirm</button>
+        <button type="submit" class="action-button" onclick="event.preventDefault(); dealCards()">Confirm</button>
       </form>
-      </div>`;
+    </div>`;
+
+  document.querySelector('.results').innerHTML = '';
+  document.querySelector('.deck').innerHTML = '';
+}
 }
