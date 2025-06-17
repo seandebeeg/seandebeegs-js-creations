d545356398afeae968107ff503706b5adce188fb
Sean de beeg <seandepew2011@gmail.com>
2025-06-17 14:23:03 -0400

    close to done but haven't tested so there could be tons of bugs

--- war/war.js
+++ war/war.js
@@ -3,11 +3,11 @@ let previousBetValue = 1;
 let playerNumber;
 
 function calculateCardsPerPlayer(){
-  playerNumber = document.getElementById('player').value
-  cardsPerPlayer = parseInt(52/ playerNumber)
+  playerNumber = document.getElementById('player').value;
+  cardsPerPlayer = parseInt(52 / playerNumber);
 }
 
-let cardsPerPlayer = calculateCardsPerPlayer()
+let cardsPerPlayer = calculateCardsPerPlayer();
 
 function createPlayer(className){
   return {
@@ -25,12 +25,18 @@ let players = [
 ];
 
 function buildDeck() {
-  const values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+  const values = [
+    'ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'
+  ];
   const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
   let deck = [];
   for (let suit of suits) {
     for (let value of values) {
-      deck.push({ name: value, suit, photo: `<img src="deck-of-cards/${value}_of_${suit}.png">` });
+      deck.push({
+        name: value,
+        suit,
+        photo: `<img src="deck-of-cards/${value}_of_${suit}.png">`
+      });
     }
   }
   return deck;
@@ -49,39 +55,78 @@ shuffle(cards);
 function dealCards(){
   players.forEach(player => {
     for(let i = 0; i <= cardsPerPlayer; i++){
-    player.cards.push(cards.shift());
+      player.cards.push(cards.shift());
     }
   });
   document.getElementById('selector').innerHTML = '';
 }
 
 function createDivs(){
-  const allPlayers = players.slice(0,playerNumber);
+  const allPlayers = players.slice(0, playerNumber);
   allPlayers.forEach((player, index) =>{
     let playerDiv = document.createElement('div');
     playerDiv.id = String(index + 1);
     playerDiv.classList.add(player.className);
     document.body.appendChild(playerDiv);
-    playerDiv.innerHTML = '<img src="card-facedown.jpg"><br>'
-    if(playerDiv.id == '1') playerDiv.innerHTML += '<button class="flip-button" onclick="flipCard();">Flip Card</button>';
+    playerDiv.innerHTML = '<img src="card-facedown.jpg"><br>';
+    if (playerDiv.id == '1') {
+      playerDiv.innerHTML += '<button class="flip-button" onclick="flipCard();">Flip Card</button>';
+    }
   });
 }
 
-function flipCard(){ //incomplete
-  const activePlayers = players.slice(0,playerNumber);
+function flipCard(existingWarPile = []){
+  const activePlayers = players.slice(0, playerNumber);
+  let pile = [];
+  pile = activePlayers.forEach(p => p.cards.shift());
   const cardValues = activePlayers.map(p => getCardValue(p));
   const maxCard = Math.max(...cardValues);
-  const winners = cardValues.filter(value => value == maxCard);
-  if(winners.length > 1){
-    
+  let winners = activePlayers
+    .map((p, idx) => ({ value: cardValues[idx], index: idx }))
+    .filter(card => card.value === maxCard);
+  if (winners.length > 1) {
+    let warCards = existingWarPile || [];
+    let warPile = [];
+    pile.forEach(card => warPile.push(pile.shift()));
+    winners.forEach(w => {
+      const player = players[w.index];
+      if (player.cards.length >= 4) {
+        player.cards.forEach(card =>{
+          for(let i = 0; i <= 3; i++){
+            warPile.push(card);
+            player.cards.splice(i);
+          }
+        });
+        warCards.push({ value: getCardValue(player), index: w.index });
+      } else {
+        warCards.push({ value: -1, index: w.index });
+        player.cards.forEach(card => warPile.push(player.cards.shift()));
+        //I'll update the HTML to show the player lost
+      }
+    });
+
+    const warMaxCard = Math.max(...warCards.map(w => w.value));
+    const warWinners = warCards.filter(w => w.value === warMaxCard);
+
+    if (warWinners.length === 1) {
+      const winnerIdx = warWinners[0].index;
+      warPile.forEach(card => players[winnerIdx].cards.push(card));
+      // I will update the html later
+    } else {
+      flipCard(warPile);
+    }
+  } else {
+    const winnerIdx = winners[0].index;
+    pile.forEach(card => players[winnerIdx].cards.push(pile.shift()));
+    // I update the html to show the win later
   }
 }
 
-function getCardValue(player){
-  const card = player.cards[0]
-  if(card.name === 'ace') return 14;
-  else if(card.name === 'king') return 13;
-  else if(card.name === 'queen') return 12;
-  else if(card.name === 'jack') return 11;
+function getCardValue(player) {
+  const card = player.cards[0];
+  if (card.name === 'ace') return 14;
+  else if (card.name === 'king') return 13;
+  else if (card.name === 'queen') return 12;
+  else if (card.name === 'jack') return 11;
   else return parseInt(card.name);
 }
